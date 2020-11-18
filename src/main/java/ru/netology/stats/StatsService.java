6c25cd0b3dda4fe3d1a438811b24706ec7a701a3
Renat Khaybullin <zabzari@gmail.com>
2020-11-18 22:31:29 +0300

    Added change

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,45 +1,128 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long calculate(long amount, boolean registered) {
-        int percent = registered ? 3 : 1;
-        long bonus = amount * percent / 100 / 100;
-        long limit = 500;
-        if (bonus > limit) {
-            bonus = limit;
+    public int sum(int[] array) {
+        int result = 0;
+        for (int amount : array) {
+            result += amount;
         }
-        return bonus;
+        return result;
+
     }
 
-    public long calculateSum(long[] purchases) {
-
-        long sum = 0;
-        for (long purchase : purchases) {
-            sum += purchase;
-        }
-        return sum;
+    public int average(int[] array) {
+        return sum(array) / array.length;
     }
 
-    public void calculateMax(long[] purchases) {
-        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
+    public int GetMaxMonth(int[] array) {
+        int max = getMax(array);
+        int result = 0;
+        int month = 0;
+        for (int amount : array) {
+            month++; //инкримент += 1
+            if (amount == max) {
+                result = month;
+            }
+        }
+        return result;
+    }
+    public int GetMinMonth(int[] array) {
+        int min = getMin(array);
+        int result = 0;
+        int month = 0;
+        for (int amount : array) {
+            month++; //инкримент += 1
+            if (amount == min) {
+                result = month;
+            }
+        }
+        return result;
+    }
+    public int getMax(int[] array) {
         int max = 0;
-        int p = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-            if (max == arr[i]) {
-                p = i;
+        for (int amount : array) {
+            if (amount > max) {
+                max = amount;
             }
         }
+        return max;
     }
 
-        public long calculateSumInMonth(long[] purchases) {
-            long sum = 141;
-            long actual;
-            actual = sum / 10;
-
-            return actual;
+    public int getMin(int[] array) {
+        int min = array[0];
+        for (int amount : array) {
+            if (amount < min) {
+                min = amount;
+            }
         }
+        return min;
+    }
+
+    public int GetMonthCountMoreThanAverage(int[] array) {
+        int average = average(array);
+        int count = 0;
+        for (int amount : array) {
+            if (amount > average) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public int GetMonthCountLessThanAverage(int[] array) {
+        int average = average(array);
+        int count = 0;
+        for (int amount : array) {
+            if (amount < average) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
 }
 
+
+//    public long calculate(long amount, boolean registered) {
+//        int percent = registered ? 3 : 1;
+//        long bonus = amount * percent / 100 / 100;
+//        long limit = 500;
+//        if (bonus > limit) {
+//            bonus = limit;
+//        }
+//        return bonus;
+//    }
+//
+//    public long sum (long[] purchases) {
+//
+//        long sum = 0;
+//        for (long purchase : purchases) {
+//            sum += purchase;
+//        }
+//        return sum;
+//    }
+//
+//    public void calculateMax(long[] purchases) {
+//        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
+//        int max = 0;
+//        int p = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > max) {
+//                max = arr[i];
+//            }
+//            if (max == arr[i]) {
+//                p = i;
+//            }
+//        }
+//    }
+//
+//        public long calculateSumInMonth(long[] purchases) {
+//            long sum = 141;
+//            long actual;
+//            actual = sum / 10;
+//
+//            return actual;
+//        }
+//}
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,122 +1,134 @@
 package ru.netology.stats;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import java.security.Provider;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
+    public StatsService service = new StatsService();
+    public int[] expectedArray = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-    @ParameterizedTest
-    @CsvSource(
-            value = {
-                    "'registered and under limit',100060,true,30",
-                    "'registered and under limit',100060,false,10"
-            },
-            delimiter = ','
-    )
-    void shouldCalculate(String test, long amount, boolean registered, long expected) {
-        StatsService service = new StatsService();
-        long actual = service.calculate(amount, registered);
+
+    @Test
+    void shouldSum() {
+        int expected = 180;
+        int actual = service.sum(expectedArray);
+
         assertEquals(expected, actual);
-    }
-
-//    Сумму всех продаж
-//    Среднюю сумму продаж в месяц
-//    Номер месяца, в котором был пик продаж (осуществлены продажи на максимальную сумму)*
-//    Номер месяца, в котором был минимум продаж (осуществлены продажи на минимальную сумму)*
-//    Кол-во месяцев, в которых продажи были ниже среднего (см. п.2)
-//    Кол-во месяцев, в которых продажи были выше среднего (см. п.2)
-
-
-    @Test
-    void shouldCalculateSum() {
-        StatsService service = new StatsService();
-        long[] purchases = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
-
-        System.out.println(service.calculateSum(purchases));
 
     }
 
     @Test
-    void calculateSumInMonth() {
-        StatsService service = new StatsService();
-        long[] purchases = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
-
-        System.out.println(service.calculateSumInMonth(purchases));
+    void shouldAverage() {
+        assertEquals(15, service.average(expectedArray));
 
     }
 
     @Test
-    void calculateMaxSum() {
-        StatsService service = new StatsService();
-        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
-        int max = 0;
-        int p = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-            if (max == arr[i]) {
-                p = i;
-            }
-        }
-        System.out.println(p);
+    void shouldGetMaxMonth() {
+        int expected = 8;
+        int actual = service.GetMaxMonth(expectedArray);
+        assertEquals(expected, actual);
+
     }
-
     @Test
-    void calculateMinSum() {
-        StatsService service = new StatsService();
-        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
-        int min = 9999;
-        int p = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-            if (min == arr[i]) {
-                p = i;
-            }
-        }
-        System.out.println(p);
+    void shouldGetMinMonth() {
+        int expected = 9;
+        int actual = service.GetMinMonth(expectedArray);
+        assertEquals(expected, actual);
+
     }
-
     @Test
-    void calculateSumElement() {
-        StatsService service = new StatsService();
-        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
-        int min = 14;
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            if (arr[i] < min) {
-                sum = sum + 1;
-            }
+    void shouldGetCountMoreThanAverage() {
+        int expected = 5;
+        int actual = service.GetMonthCountMoreThanAverage(expectedArray);
+        assertEquals(expected, actual);
 
-
-        }
-        System.out.println(sum);
     }
-
     @Test
-    void calculateSumElementMax() {
-        StatsService service = new StatsService();
-        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
-        int min = 14;
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            if (arr[i] > min) {
-                sum = sum+1;
-            }
+    void shouldGetCountLessThanAverage() {
+        int expected = 5;
+        int actual = service.GetMonthCountLessThanAverage(expectedArray);
+        assertEquals(expected, actual);
 
-
-        }
-        System.out.println(sum);
     }
 }
-
+//
+//    @Test
+//    void calculateSumInMonth() {
+//        StatsService service = new StatsService();
+//        long[] purchases = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
+//
+//        System.out.println(service.calculateSumInMonth(purchases));
+//
+//    }
+//
+//    @Test
+//    void calculateMaxSum() {
+//        StatsService service = new StatsService();
+//        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
+//        int max = 0;
+//        int p = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > max) {
+//                max = arr[i];
+//            }
+//            if (max == arr[i]) {
+//                p = i;
+//            }
+//        }
+//        System.out.println(p);
+//    }
+//
+//    @Test
+//    void calculateMinSum() {
+//        StatsService service = new StatsService();
+//        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
+//        int min = 9999;
+//        int p = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] < min) {
+//                min = arr[i];
+//            }
+//            if (min == arr[i]) {
+//                p = i;
+//            }
+//        }
+//        System.out.println(p);
+//    }
+//
+//    @Test
+//    void calculateSumElement() {
+//        StatsService service = new StatsService();
+//        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
+//        int min = 14;
+//        int sum = 0;
+//        for (int i = 0; i < 10; i++) {
+//            if (arr[i] < min) {
+//                sum = sum + 1;
+//            }
+//
+//
+//        }
+//        System.out.println(sum);
+//    }
+//
+//    @Test
+//    void calculateSumElementMax() {
+//        StatsService service = new StatsService();
+//        int arr[] = {8, 15, 13, 15, 17, 20, 7, 14, 14, 18};
+//        int min = 14;
+//        int sum = 0;
+//        for (int i = 0; i < 10; i++) {
+//            if (arr[i] > min) {
+//                sum = sum+1;
+//            }
+//
+//
+//        }
+//        System.out.println(sum);
+//    }
+//}
 
 
 
